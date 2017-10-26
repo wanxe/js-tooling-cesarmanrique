7b396f100aabc50ab40df903eb81e8440964bdae
JNieto <jorge.bbz@gmail.com>
2017-10-26 23:01:32 +0100

    Paso 6

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,1 +1,29 @@
-export default (text) => console.log(`debug %c ${text}`, 'background-color: #111; color: yellow; font-size: 14px;')
+import './style.css'
+
+export default {
+  init () {
+    // buscamos todos los disparadores que en este caso serán los botones dentro de accordion-item
+    const triggers = document.querySelectorAll('.accordion-item .trigger')
+    // ahora debemos escuchar evento click de cada uno de ellos
+    triggers.forEach(trigger => trigger.addEventListener('click', this.handleClick))
+  },
+  handleClick (e) {
+    e.stopImmediatePropagation()
+    const button = this
+    button.classList.toggle('open')
+    const content = button.nextElementSibling
+    const isOpened = content.style.maxHeight
+
+    // en caso de que tenga maxheight querrá decir
+    // que está abierto
+    if (isOpened) {
+      // lo debemos cerrar
+      content.style.maxHeight = null
+    }
+
+    if (!isOpened) {
+      // lo abrimos
+      content.style.maxHeight = `${content.scrollHeight}px`
+    }
+  }
+}
